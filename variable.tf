eb7a52d5c7bcf3ad71c15a7373814e37ee2666ba
jose liza <joselizagaravito@gmail.com>
2024-12-14 09:40:57 -0500

    primera actualizacion

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "location" {
+    default = "East US"
+}
+
+variable "resource_group_name" {
+    default = "jenkins-aks-rg"
+}
+
+variable "subscription_id" {
+    default = "c9f87aca-4d37-4036-9ca6-42356791a21c"
+}
+variable "client_id" {
+    default = ""
+}
+variable "client_secret" {
+    default = ""
+}
+variable "tenant_id" {
+    default = "799bf33c-dbbf-40f4-842a-ebce893cc3af"
+}

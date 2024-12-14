927f3ece533975fed626c93f939734a98eab7da8
jose liza <joselizagaravito@gmail.com>
2024-12-14 10:09:47 -0500

    agregar variables y jenkinsfile

--- variable.tf
+++ variable.tf
@@ -6,15 +6,7 @@ variable "resource_group_name" {
     default = "jenkins-aks-rg"
 }
 
-variable "subscription_id" {
-    default = "c9f87aca-4d37-4036-9ca6-42356791a21c"
-}
-variable "client_id" {
-    default = "7256f847-2a0c-4224-b7a4-5c91efeaf900"
-}
-variable "client_secret" {
-    default = "748255f0-ae29-4148-b82a-f59b04ca6b16"
-}
-variable "tenant_id" {
-    default = "799bf33c-dbbf-40f4-842a-ebce893cc3af"
-}
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
